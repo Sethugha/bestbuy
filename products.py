771eb3abc89f7bfa3ccae51c08b18104cc387527
Sethugha <gerd.furmanek@gmx.net>
2025-05-04 20:28:48 +0200

    added docstrings

--- products.py
+++ products.py
@@ -1,37 +1,102 @@
 class Product:
+    """
+    class for products
+
+    Attributes:
+        name: str
+            product name
+        price: float
+            product price
+        quantity: int
+            quantity of products in stock
+        active: boolean
+            True if product can be sold
+
+    Methods:
+    get_quantity()
+        returns quantity of product in stock
+
+    set_quantity(quantity)
+        sets the quantity in stock, overwrites old value
+
+    is_active()
+        returns if active is True or not
+
+    activate()
+        toggles active-attribute to True
+
+    deactivate()
+        toggles active-attribute to False
+
+    show()
+        returns product name, price and quantity in stock
+
+    buy(quantity)
+        desired quantity is subtracted from stock. If stock is insufficient,
+        quantity is reduced to stock value.
+        If stock reaches 0, product will be deactivated.
+        Returns quantity multiplied with price as float.
+    """
 
     def __init__(self, name, price, quantity):
+        """
+        constructs all necessary attributes for a product object
+        :param name: product name, str
+        :param price: product price, float
+        :param quantity: quantity in stock, int
+        """
         if name and price and quantity:
             if float(price) < 0 or int(quantity) < 0:
                 raise Exception("Sorry, no numbers below zero.")
             self.name = name
             self.price = float(price)
             self.quantity = int(quantity)
-            self.active = True
+            if self.quantity > 0:
+                self.active = True
+            else:
+                self.active = False
         else:
             raise Exception("Sorry, we do not sell No-Name products! Please apply a name.")
 
 
     def get_quantity(self):
+        """returns quantity of product in stock"""
         return self.quantity
 
+
     def set_quantity(self, quantity):
+        """sets quantity of product in stock. Overwrites old value."""
         self.quantity = quantity
 
+
     def is_active(self):
+        """returns if product is available"""
         return self.active
 
+
     def activate(self):
+        """sets product as available"""
         self.activate = True
 
+
     def deactivate(self):
+        """sets product as not available"""
         self.active = False
 
+
     def show(self):
+        """returns products main attributes"""
         return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
 
 
     def buy(self, quantity):
+        """desired quantity is subtracted from stock. If stock is insufficient,
+        quantity is reduced to stock value.
+        If stock reaches 0, product will be deactivated.
+        Returns quantity multiplied with price as float.
+        :param quantity: Number of ordered products
+        :returns: total price of sold products
+        """
         if quantity < 0:
             raise Exception("No quantities below zero.")
         if self.quantity >= quantity:
--- store.py
+++ store.py
@@ -1,12 +1,41 @@
 from products import Product
 
 class Store:
+    """
+    class for stores
 
+    Attributes:
+        product_list: list of product objects
+
+    Methods:
+        add_product(product_object)
+            adds product object to store product list
+
+        remove_product(product_object)
+            removes product from store product list
+
+        get_total_quantity()
+            returns sum of all product items in stock
+
+        get_all_products()
+            returns a list of active products.
+            A product in stock is active, aka available
+
+        order(shopping_list)
+            processes a shopping list composed of
+            product/amount pairs. Triggers products´
+            buy(quantity) method
+            returns total price of all orders in list.
+    """
     def __init__(self, product_list):
+        """constructs a store with the products in product_list
+        attributes: product_list, list of product objects
+        """
         self.product_list = product_list
 
 
     def add_product(self, product):
+        """adds product object to store if not already present"""
         if product not in self.product_list:
             self.product_list.append(product)
         else:
@@ -14,6 +43,7 @@ class Store:
 
 
     def remove_product(self, product):
+        """removes product from store"""
         if product not in self.product_list:
             raise Exception("Product not in store. Maybe a typo?")
         else:
@@ -21,19 +51,30 @@ class Store:
 
 
     def get_total_quantity(self):
+        """returns total quantity of all items in stock"""
         total_quantity = 0
         for product in self.product_list:
             total_quantity += product.get_quantity()
         return total_quantity
 
+
     def get_all_products(self):
+        """returns a list ob available products"""
         active_products = []
         for product in self.product_list:
             if product.is_active():
                 active_products.append(product)
         return active_products
 
+
     def order(self, shopping_list):
+        """
+        processes a list of product/amount pairs
+        subtracts orders from stock, deactivates sold-out products
+        and calculates the total price of all orders
+        :parameter: Shopping list, containing product/amount pairs
+        :returns: total price, float
+        """
         total_price = 0
         for product, quantity in shopping_list:
             if product in self.product_list:
@@ -41,6 +82,7 @@ class Store:
             else:
                 continue
         return total_price
+
 
 def main():
     product_list = [Product("MacBook Air M2", price=1450, quantity=100),
